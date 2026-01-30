1695d3557231eea63641169c9c3b9eb895061b46
Gabriel Windlin <gawindlin@gmail.com>
2026-01-30 16:33:30 +0100

    old names

--- basics/hello.c
+++
@@ -1,9 +0,0 @@
-/* hello.c */
-#include <stdio.h>
-
-/* basic structure common in all c programs */
-/* all programs consist of functions and variables */
-int main() {
-    printf("Hello World!\n");
-    return 0;
-}
