53f7f2b3d90a90ef07f7b3521f7ce38b4cee7857
Gabriel Windlin <gawindlin@gmail.com>
2026-01-30 16:27:40 +0100

    1.1

---
+++ 1-easy/1.1-if.c
@@ -0,0 +1,17 @@
+/* if.c */
+#include <stdio.h>
+
+int main() {
+    int x;
+
+    printf("Choose a number:\n");
+    scanf("%d", &x);
+
+    if (x == 5) {
+        printf("You won a million dollars!\n");
+    } else {
+        printf("You owe me a million dollars!\n");
+    }
+
+    return 0;
+}
