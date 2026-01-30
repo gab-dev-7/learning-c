3071b7f558df846ebc6d322fc738f5aef1e07b30
Gabriel Windlin <gawindlin@gmail.com>
2026-01-30 16:27:50 +0100

    1.2

---
+++ 1-easy/1.2-if_2.c
@@ -0,0 +1,29 @@
+/* if_2.c */
+#include <stdio.h>
+
+int main() {
+
+    /* another demonstration of if statements */
+
+    int x;
+    int velocity;
+
+    printf("Let's talk about cars!\n");
+    printf("Do you use the metric or imperial system? Choose 1 or 0 respectively.\n");
+
+    scanf("%d", &x);
+
+    printf("What was the fastest per hour you have ever gone?");
+    scanf("%d", &velocity);
+
+    int velocityInSeconds;
+
+    if (x == 1) {
+        velocityInSeconds = (float)velocity / 3.6;
+        printf("%d kmh??? that is about %d m/s\n", velocity, velocityInSeconds);
+    } else if (x == 0) {
+        printf("%d mph??? that is about 83947 squared inches of eagles and o.j.\n", velocity);
+    }
+
+    return 0;
+}
